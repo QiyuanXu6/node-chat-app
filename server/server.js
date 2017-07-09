1372ce6c7f9f956ffc4b147ce956f89458bc1c52
xqqyyy <qq649981156@163.com>
2017-07-09 15:57:12 +0800

    AA

--- server/server.js
+++ server/server.js
@@ -15,18 +15,38 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) => {
   console.log('New user connected!');
 
+   socket.emit('newMessage', {
+     from: 'admin',
+     text: 'Welcome to the chat app',
+     createdAt: new Date().getTime()
+   });
+
+   socket.broadcast.emit('newMessage', {
+     from: 'admin',
+     text: 'New user joined',
+     createdAt: new Date().getTime()
+   });
+
+
   socket.on('disconnect', () => {
     console.log('User disconnected!');
   });
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
-      createAt: new Date().getTime()
+      createdAt: new Date().getTime()
     });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createAt: new Date().getTime()
+    // });
   });
+
 });
 
 
